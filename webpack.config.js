5653f0b3ba679727cbab94ce26d53fbe6478aa66
Shaowei <zsw22@vip.qq.com>
2017-12-09 16:18:38 +1100

    add css support and babel

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+module.exports = {
+	devtool: "cheap-module-eval-source-map",
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: "bundle.js",
+		publicPath: ""
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: [
+					{loader: 'style-loader'},
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+							modules: true,
+							localIdentName: '[name]__[local]__[hash:base64:5]'
+						}
+					},
+					{
+						loader: "postcss-loader",
+						options: {
+							ident: 'postcss',
+							plugins: () => [
+								autoprefixer({
+									browsers: [
+										">1%",
+										"last 2 versions"
+									]
+								})
+							]
+						}
+					}
+				]
+			}
+		]
+	}
+}
